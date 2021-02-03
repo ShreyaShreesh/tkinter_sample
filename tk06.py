125aa75b855114b25c46b26ce574385f83eb99ad
SHREYA <shreyagopal17@gmail.com>
2021-02-03 14:02:48 +0900

    username and email

--- tk06.py
+++ tk06.py
@@ -8,7 +8,7 @@ def action_btn_press():
 
 root = tk.Tk()
 root.title('Widget creation')
-root.geometry('350x150')
+root.geometry('350x15')
 lb = tk.Label(text='Label-1')
 bt = tk.Button(text='BUTTON-1',command=action_btn_press)
 lb.pack()
